f067303b77bc6f09e7c344ed5f2f792f9f02e9f7
Денис <denis.darwin12@gmail.com>
2019-10-24 15:00:48 +0300

    Добавил все валютные пары, которые есть в API

--- src/components/CurrencyPairs/CurrencyPairs.jsx
+++ src/components/CurrencyPairs/CurrencyPairs.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
-import { thisExpression } from '@babel/types';
 
+let a = `RUB_EUR_USD_UAH_BBD_BTN_BND_XAF_CUP_FKP_GIP_HUF_IRR_JMD_AUD_LAK_
+        LYD_MKD_XOF_NZD_OMR_PGK_RWF_WST_RSD_SEK_TZS_AMD_BSD_BAM_CVE_CNY_
+        CRC_CZK_ERN_GEL_HTG_INR_JOD_KRW_LBP_MWK_MRO_MZN_ANG_PEN_QAR_STD_
+        SLL_SOS_SDG_SYP_AOA_AWG_BHD_BZD_BWP_BIF_KYD_COP_DKK_GTQ_HNL_IDR_
+        ILS_KZT_KWD_LSL_MYR_MUR_MNT_MMK_NGN_PAB_PHP_RON_SAR_SGD_ZAR_SRD_
+        TWD_TOP_VEF_DZD_ARS_AZN_BYR_BOB_BGN_CAD_CLP_CDF_DOP_FJD_GMD_GYD_
+        ISK_IQD_JPY_KPW_LVL_CHF_MGA_MDL_MAD_NPR_NIO_PKR_PYG_SHP_SCR_SBD_
+        LKR_THB_TRY_AED_VUV_YER_AFN_BDT_BRL_KHR_KMF_HRK_DJF_EGP_ETB_XPF_
+        GHS_GNF_HKD_XDR_KES_KGS_LRD_MOP_MVR_MXN_NAD_NOK_PLN_SZL_TJS_
+        TTD_UGX_UYU_VND_TND_UZS_TMT_GBP_ZMW_BTC_BYN_BMD_GGP_CLF_CUC_
+        IMP_JEP_SVC_ZMK_XAG_ZWL_ALL_XCD`;
+const pair = a.split('_');
+console.log(pair);
 
+const elements = pair.map((item) => {
+    return (
+        <option key={item} value={item}>
+            {item}
+        </option>
+    )
+});
 
 class CurrencyPairs extends React.Component {
 
@@ -21,22 +40,16 @@ class CurrencyPairs extends React.Component {
     }
     
     render() {
-       const { currencyIn, currencyOut} = this.props
-
         return (
             <div>
                 <div className="box">
                     <select onChange = { this.handleChangeIn } value={this.state.currencyIn}>
-                        <option value="RUB">RUB</option>
-                        <option value="USD">USD</option>
-                        <option value="UAH">UAH</option>
+                        {elements}
                     </select>
                 </div>
                 <div className="box">
                     <select onChange = { this.handleChangeOut } value={this.state.currencyOut}>
-                        <option value="USD">USD</option>
-                        <option value="RUB">RUB</option>  
-                        <option value="UAH">UAH</option>
+                        {elements}
                     </select>
                 </div>
             </div>
